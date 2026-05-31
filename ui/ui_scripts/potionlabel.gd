f890dc4e811593d24811ac432c9dc78afbcb3447
kyryll-ts <unitykinggodot@gmail.com>
2026-05-31 09:44:41 -0600

    More feedback on potions.
    I added a error sound now when a potion is not able to be drank, in an instance when the player has no potion or when their health is at 100%

--- ui/ui_scripts/potionlabel.gd
+++ ui/ui_scripts/potionlabel.gd
@@ -1,11 +1,20 @@
 extends Label
 @onready var drinking_sound = $DrinkingSound
+@onready var error_sound = $Error
 
 func _process(delta: float) -> void:
 	self.text = "X" + str(Global.potion_amount)
 
-	if Input.is_action_just_pressed("drink_potion") and Global.potion_amount > 0:
-		if Global.player_life < 100:
-			Global.player_life = min(Global.player_life + Global.potion_increase, 100)
-			drinking_sound.play()
-			Global.potion_amount -= 1
+	if Input.is_action_just_pressed("drink_potion"):
+		if Global.potion_amount > 0:
+		
+			if Global.player_life < 100:
+				Global.player_life = min(Global.player_life + Global.potion_increase, 100)
+				drinking_sound.play()
+				Global.potion_amount -= 1
+			else:
+				error_sound.play()
+		else:
+			error_sound.play()
+	
+	
