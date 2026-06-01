8463040375d1e139d2cdac1e1d37e9dcb25eb877
Visclodev <yobobis@gmail.com>
2026-06-01 22:49:38 +0200

    slower death effect

--- death_fx/death_fx.gd
+++ death_fx/death_fx.gd
@@ -6,9 +6,9 @@ func _ready() -> void:
 	$GPUParticles2D.emitting = true
 	var tween = create_tween()
 	tween.set_parallel(true)
-	tween.tween_method(set_shader_value.bind("progress_fall"), 0.0, 1.0, 2.0)
-	tween.tween_method(set_shader_value.bind("progress_scatter"), 0.0, 1.0, 2.0)
-	tween.tween_method(set_shader_value.bind("progress_fade"), 0.0, 1.0, 1.0)
+	tween.tween_method(set_shader_value.bind("progress_fall"), 0.0, 1.0, 4.0)
+	tween.tween_method(set_shader_value.bind("progress_scatter"), 0.0, 1.0, 4.0)
+	tween.tween_method(set_shader_value.bind("progress_fade"), 0.0, 1.0, 3.0)
 
 
 func set_shader_value(value, key):
