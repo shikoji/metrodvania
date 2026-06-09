956f2c139cc48c61a7cb5a034b46109a5a3e6f06
Visclodev <yobobis@gmail.com>
2026-06-09 18:33:05 +0200

    ground bombs completed + sfx for cells

--- bombs/explosions/explosion.gd
+++ bombs/explosions/explosion.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
+const GROUND_EXPLOSION = preload("res://bombs/explosions/ground_particles.tscn")
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$GPUParticles2D.emitting = true
 	$GPUParticles2D2.emitting = true
@@ -13,14 +13,8 @@ func _physics_process(delta: float) -> void:
 	if tick == 2:
 		for body in get_overlapping_bodies():
 			if body is TileMapLayer:
-				var cells = []
-				var to_erase = []
-				var cell = body.local_to_map(position)
-				cells.append_array(get_surrounding_cells(cell))
-				for c in cells:
-					to_erase.append_array(get_surrounding_cells(c))
-				for c in to_erase:
-					body.set_cell(c)
+				break_cells(body)
+
 			if body is Player:
 				body.take_damage(20)
 			if body.is_in_group("enemies"):
@@ -34,6 +28,24 @@ func _physics_process(delta: float) -> void:
 			if body is RigidBody2D:
 				var direction = global_position.direction_to(body.global_position)
 				body.apply_central_impulse(direction * 1000.0)
+
+func break_cells(body: TileMapLayer):
+	var used_cells = body.get_used_cells()
+	var cells = []
+	var to_erase = []
+	var cell = body.local_to_map(position)
+	cells.append_array(get_surrounding_cells(cell))
+	for c in cells:
+		to_erase.append_array(get_surrounding_cells(c))
+	for c in to_erase:
+		if c in used_cells:
+			var local_pos = body.map_to_local(c)
+			var instance = GROUND_EXPLOSION.instantiate()
+			instance.position = local_pos
+			instance.emitting = true
+			instance.connect("finished", instance.queue_free)
+			add_sibling(instance)
+			body.set_cell(c)
 
 func get_surrounding_cells(cell: Vector2i):
 	var res = []
--- bombs/ground_bomb/ground_bomb.gd
+++ bombs/ground_bomb/ground_bomb.gd
@@ -2,15 +2,40 @@ extends RigidBody2D
 class_name GroundBomb
 
 const EXPLOSION = preload("res://bombs/explosions/explosion.tscn")
+const GROUND_EXPLOSION = preload("res://bombs/explosions/ground_particles.tscn")
 
 func explode():
 	# get the tilemap
-	
+	var tilemap: TileMapLayer
+	for node in get_tree().current_scene.get_children():
+		if node is TileMapLayer:
+			tilemap = node
+			break
+
 	# place ground terrain
+	var cell = tilemap.local_to_map(position)
+	var cells = [cell]
+	cells.append(cell + Vector2i.LEFT)
+	cells.append(cell + Vector2i.RIGHT)
+	tilemap.set_cells_terrain_connect(cells, 0, 0)
 	
 	# play some sfx
-	var instance = EXPLOSION.instantiate()
-	instance.position = position
-	add_sibling(instance)
+	var sound = AudioStreamPlayer2D.new()
+	sound.position = position
+	sound.stream = load("res://bombs/ground_bomb/creating_ground.wav")
+	add_sibling(sound)
+	sound.play()
+	sound.connect("finished", sound.queue_free)
+	
+	for c in cells:
+		var local_pos = tilemap.map_to_local(c)
+		var instance = GROUND_EXPLOSION.instantiate()
+		instance.position = local_pos
+		instance.emitting = true
+		instance.connect("finished", instance.queue_free)
+		add_sibling(instance)
+	#var instance = EXPLOSION.instantiate()
+	#instance.position = position
+	#add_sibling(instance)
 	
 	queue_free()
