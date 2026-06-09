1aa7cbf8af0d34b7f01388384fcc9c52a173f0ac
Visclodev <yobobis@gmail.com>
2026-06-09 11:01:57 +0200

    bombs breaking terrain

--- bombs/bomb/bomb.gd
+++ bombs/bomb/bomb.gd
@@ -1,9 +1,15 @@
 extends RigidBody2D
 class_name Bomb
 
+const EXPLOSION = preload("res://bombs/explosions/explosion.tscn")
+
 func _ready() -> void:
 	pass
 
 func explode():
+	var instance = EXPLOSION.instantiate()
+	instance.position = position
+	add_sibling(instance)
+	
 	# place bomb explosion code here
 	queue_free()
---
+++ bombs/explosions/explosion.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$GPUParticles2D.emitting = true
+	$GPUParticles2D2.emitting = true
+
+var tick = 0
+
+func _physics_process(delta: float) -> void:
+	tick += 1
+	if tick == 2:
+		for body in get_overlapping_bodies():
+			print(body)
+			if body is TileMapLayer:
+				var cells = []
+				var to_erase = []
+				var cell = body.local_to_map(position)
+				cells.append_array(get_surrounding_cells(cell))
+				for c in cells:
+					to_erase.append_array(get_surrounding_cells(c))
+				for c in to_erase:
+					body.set_cell(c)
+			
+
+func get_surrounding_cells(cell: Vector2i):
+	var res = []
+	res.append(cell)
+	res.append(cell + Vector2i.UP)
+	res.append(cell + Vector2i.DOWN)
+	res.append(cell + Vector2i.LEFT)
+	res.append(cell + Vector2i.RIGHT)
+	return res
